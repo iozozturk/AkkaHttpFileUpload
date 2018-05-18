5d8edcb5d30b8345b679f569e5ae1b00e8983f5c
Ismet Ozozturk <ismet.ozozturk@veon.com>
2018-05-18 12:10:47 +0200

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,46 @@
+import java.io.File
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.directives.FileInfo
+import akka.stream.ActorMaterializer
+
+import scala.io.StdIn
+
+object Main extends App {
+  implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system.dispatcher
+
+  def tempDestination(fileInfo: FileInfo): File =
+    File.createTempFile(fileInfo.fileName, ".tmp")
+
+  val route =
+    path("hello") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    } ~
+      post {
+        path("fileUpload") {
+          storeUploadedFile("csv", tempDestination) {
+            case (metadata, file) =>
+              // do something with the file and file metadata ...
+              file.delete()
+              complete(OK)
+          }
+        }
+      }
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+}
